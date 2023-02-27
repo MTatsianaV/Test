b8e6f423cd75fe06ca74dc744c5dd4f4633c8793
Tatsiana Marozava <mortatval@gmail.com>
2023-02-27 11:38:39 +0300

    Добавили метод для создание строкового массива через ввод с клавиатуры

--- test1/Program.cs
+++ test1/Program.cs
@@ -9,3 +9,18 @@
 ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
 ["1234", "1567", "-2", "computer scince"] -> ["-2"]
 ["Russia", "Denmark", "Kazan"] -> [" "] */
+Console.WriteLine("Введите количество строк массива:");
+int index = Convert.ToInt32(Console.ReadLine());
+string[] CreateStringMass(int ind)
+{
+    string[] stringArray = new string[index];
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        Console.WriteLine("Введите строку:");
+        stringArray[i] = Console.ReadLine();
+    }
+    return stringArray;
+}
+
+string[] arr = CreateStringMass(index);
+
