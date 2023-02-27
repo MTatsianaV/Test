3cf4ccdf22ab25071f006394227a6215255f8e96
Tatsiana Marozava <mortatval@gmail.com>
2023-02-27 11:40:40 +0300

    Добавили метод для отоюражения полученного строкового массива в консоли

--- test1/Program.cs
+++ test1/Program.cs
@@ -21,6 +21,16 @@ string[] CreateStringMass(int ind)
     }
     return stringArray;
 }
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+        if (i < array.Length - 1) Console.Write($", ");
+    }
+    Console.WriteLine("]");
+}
 
 string[] arr = CreateStringMass(index);
-
+PrintArray(arr);
